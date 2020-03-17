6f9712fd0bc327513750a0abc2db3d1c476e2570
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2020-03-17 19:40:36 +0100

    Update minimal project settings to work with latest derex.runner version

--- tests/fixtures/minimal/settings/base.py
+++ tests/fixtures/minimal/settings/base.py
@@ -1,15 +1,6 @@
-import os
-
-SERVICE_VARIANT = os.environ["SERVICE_VARIANT"]
-assert SERVICE_VARIANT in ("lms", "cms")
-exec(
-    "from {}.envs.derex.base import *".format(SERVICE_VARIANT), globals(), locals(),
-)
-
-# Id of the site fixture to use, instead of looking up the hostname
-SITE_ID = 1
+from .derex import *  # noqa
 
 COMMENTS_SERVICE_URL = "http://forum:4567"
 COMMENTS_SERVICE_KEY = "forumapikey"
 
-FEATURES["ENABLE_DISCUSSION_SERVICE"] = True  # type: ignore # noqa: F821
+FEATURES["ENABLE_DISCUSSION_SERVICE"] = True  # type: ignore # noqa: F405
