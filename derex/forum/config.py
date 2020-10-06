48f6b917759b2f18e62b62b5c2cbcf5f925fc0ae
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2020-10-06 12:52:51 +0200

    Properly format root user and password in mongodb connection url

--- derex/forum/config.py
+++ derex/forum/config.py
@@ -1,9 +1,12 @@
+import urllib.parse
 from pathlib import Path
 from typing import Dict, List, Union
 
 import pkg_resources
 
 from derex import runner  # type: ignore
+from derex.runner.constants import MONGODB_ROOT_USER
+from derex.runner.mongodb import MONGODB_ROOT_PASSWORD
 from derex.runner.project import Project
 from jinja2 import Template
 
@@ -19,8 +22,15 @@ def generate_local_docker_compose(project: Project) -> Path:
     forum_docker_image = project.config.get(
         "forum_docker_image", f"derex/forum:{project.openedx_version.name}"
     )
+    mongodb_root_user = urllib.parse.quote_plus(MONGODB_ROOT_USER)
+    mongodb_root_password = urllib.parse.quote_plus(MONGODB_ROOT_PASSWORD)
     tmpl = Template(template_path.read_text())
-    text = tmpl.render(project=project, forum_docker_image=forum_docker_image)
+    text = tmpl.render(
+        project=project,
+        forum_docker_image=forum_docker_image,
+        MONGODB_ROOT_USER=mongodb_root_user,
+        MONGODB_ROOT_PASSWORD=mongodb_root_password,
+    )
     local_compose_path.write_text(text)
     return local_compose_path
 
