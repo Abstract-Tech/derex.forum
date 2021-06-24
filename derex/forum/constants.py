c56a5ca00e7beb821eb1018fa74806729141a775
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2021-06-24 18:13:58 +0200

    Use Github Container Registry inplace of Dockerhub

--- derex/forum/constants.py
+++ derex/forum/constants.py
@@ -8,20 +8,20 @@ class ForumVersions(Enum):
         "forum_repository": "https://github.com/edx/cs_comments_service.git",
         "forum_version": "open-release/ironwood.master",
         "forum_release": "ironwood",
-        "docker_image_prefix": "derex/forum-ironwood",
+        "docker_image_prefix": "ghcr.io/Abstract-Tech/derex-forum-ironwood",
         "ruby_version": "2.4.1",
     }
     juniper = {
         "forum_repository": "https://github.com/edx/cs_comments_service.git",
         "forum_version": "open-release/juniper.master",
         "forum_release": "juniper",
-        "docker_image_prefix": "derex/forum-juniper",
+        "docker_image_prefix": "ghcr.io/Abstract-Tech/derex-forum-juniper",
         "ruby_version": "2.5.7",
     }
     koa = {
         "forum_repository": "https://github.com/edx/cs_comments_service.git",
         "forum_version": "open-release/koa.master",
         "forum_release": "koa",
-        "docker_image_prefix": "derex/forum-koa",
+        "docker_image_prefix": "ghcr.io/Abstract-Tech/derex-forum-koa",
         "ruby_version": "2.5.7",
     }
